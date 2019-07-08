41673c3e4e1273af50b2e0c612667eb882e69878
makerling <makerling@users.noreply.github.com>
2019-07-08 11:29:22 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -5,9 +5,11 @@ REM installs Boxstarter and Chocolatey from web - ensures latest version is alwa
 
 powershell -NoProfile -ExecutionPolicy bypass -command ". { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; Get-Boxstarter -Force"
 
-REM installing/running Boxstarter package from gist see: https://boxstarter.org/InstallingPackages
+REM installing/running Boxstarter package from raw file see: https://boxstarter.org/InstallingPackages
 
-powershell -NoProfile -ExecutionPolicy bypass -command "Install-BoxstarterPackage -DisableReboots -PackageName https://gist.githubusercontent.com/makerling/6af68b1c6bc636549b36ec19c41575d7/raw/3d924ff1b7ea9000b2c310ae07d04b7ed1674ce0/FLEx9-dev-setup.ps1"
+REM powershell -NoProfile -ExecutionPolicy bypass -command "Install-BoxstarterPackage -DisableReboots -PackageName https://gist.githubusercontent.com/makerling/6af68b1c6bc636549b36ec19c41575d7/raw/3d924ff1b7ea9000b2c310ae07d04b7ed1674ce0/FLEx9-dev-setup.ps1"
+
+powershell -NoProfile -ExecutionPolicy bypass -command "Install-BoxstarterPackage -DisableReboots -PackageName https://raw.githubusercontent.com/makerling/flexdevsetup/master/boxstartercommands.ps1" 
 
 echo "Script has completed, you can close the console and restart your machine."
 
