c1d77932f43872f753be9cd0a4ee7e9baf24a841
makerling <makerling@users.noreply.github.com>
2019-07-11 11:07:26 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,24 +1,30 @@
 @echo off
 
-REM ToDo: check if admin, else break, run boxstarter script from relative file rather than github text file (users an edit it easier)
-echo "Downloading latest version of script, running shortly..."
+REM Detecting Admin permissions
+REM Ref: https://stackoverflow.com/questions/4051883/batch-script-how-to-check-for-admin-rights#11995662
+echo "Administrative permissions required. Detecting permissions..."
 
-REM need to run as admin, Windows 10 might block this from running, need to disable SmartScreen (click 'more info' click 'run anyways')
-REM installs Boxstarter and Chocolatey from web - ensures latest version is always installed
+net session >nul 2>&1
+if %errorLevel% == 0 (
+  echo "Success: Administrative permissions confirmed, continuing..."
+  echo
+  echo "Downloading latest version of script, running shortly..."
 
-powershell -NoProfile -ExecutionPolicy bypass -command ". { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; Get-Boxstarter -Force"
+  REM need to run as admin, Windows 10 might block this from running, need to disable SmartScreen (click 'more info' click 'run anyways')
+  REM installs Boxstarter and Chocolatey from web - ensures latest version is always installed
+  powershell -NoProfile -ExecutionPolicy bypass -command ". { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; Get-Boxstarter -Force"
 
-echo
-echo "updating environment to recognize boxstarter install"
-REM syntax for cmd: https://github.com/chocolatey/choco/issues/1461
-REM reference: https://chocolatey.org/docs/helpers-update-session-environment
+  echo
+  echo "updating environment to recognize boxstarter install"
+  REM syntax for cmd: https://github.com/chocolatey/choco/issues/1461
+  REM reference: https://chocolatey.org/docs/helpers-update-session-environment
+  call refreshenv.cmd
 
-call refreshenv.cmd
+  REM installing/running Boxstarter package from local powershell script file see: https://boxstarter.org/InstallingPackages
+  powershell -NoProfile -ExecutionPolicy bypass -command "Install-BoxstarterPackage -PackageName boxstartercommands.ps1 -DisableReboots" 
 
-REM installing/running Boxstarter package from github text file see: https://boxstarter.org/InstallingPackages
-
-powershell -NoProfile -ExecutionPolicy bypass -command "Install-BoxstarterPackage -DisableReboots -PackageName boxstartercommands.ps1" 
-
-echo "Script has completed, you can close the console and restart your machine."
-
+  echo "Script has completed, you can close the console and restart your machine."  
+) else (
+        echo "Failure: Current permissions inadequate, close terminal and restart script as admin."
+    )
 PAUSE
