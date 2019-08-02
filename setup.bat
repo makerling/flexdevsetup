8dfe12d58a98e4df2fb2a43dd5bc4e38dedf13c9
makerling <makerling@users.noreply.github.com>
2019-08-02 15:43:24 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -7,14 +7,14 @@ echo Administrative permissions required. Detecting permissions...
 net session >nul 2>&1
 if %errorLevel% == 0 (
   echo Success: Administrative permissions confirmed, continuing...
-  echo
+  echo ""
   echo Downloading latest version of script, running shortly...
 
   REM need to run as admin, Windows 10 might block this from running, need to disable SmartScreen (click 'more info' click 'run anyways')
   REM installs Boxstarter and Chocolatey from web - ensures latest version is always installed
   powershell -NoProfile -ExecutionPolicy bypass -command ". { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; Get-Boxstarter -Force"
 
-  echo
+  echo ""
   echo updating environment to recognize boxstarter install
   REM syntax for cmd: https://github.com/chocolatey/choco/issues/1461
   REM reference: https://chocolatey.org/docs/helpers-update-session-environment
